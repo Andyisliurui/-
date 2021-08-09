31312ccb821af0a9209d5a7ee6d264fea1359329
liuhuohuo <846461870@qq.com>
2021-08-09 10:29:11 +0800

    第一次提交

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 116);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "点击选取文件夹（或拖拽至此）";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.DragDrop += new System.Windows.Forms.DragEventHandler(this.button1_DragDrop);
+            this.button1.DragEnter += new System.Windows.Forms.DragEventHandler(this.button1_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 140);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "文件夹锁-By 刘火火";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public delegate void TransfDelegate1(Boolean value);
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(string fname)
+        {
+            InitializeComponent();
+            // TODO: Complete member initialization
+            this.fname = fname;
+            foreach (Control c in this.Controls)  //使用循环获取页面中的所有控件
+            {
+                if (c is TextBox)     //如果是TextBox控件，则添加事件
+                {
+                    TextBox tb1 = c as TextBox;
+                    c.KeyDown += new KeyEventHandler(Key_Down);
+                }
+            }
+        }
+        private void Key_Down(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                SendKeys.Send("{Tab}");//向活动应用程序发送击键 
+            }
+
+        }
+        public event TransfDelegate1 TransfEvent1;
+        bool pwdType = false;
+        //创建数据对象
+        Data getHost = new WindowsFormsApplication3.Data();
+        List<pwd> dd = new List<pwd>();
+        private string fname;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == this.textBox2.Text && this.textBox1.Text != "")
+            {
+                
+                int a = 0;
+                try { dd = getHost.zGetpwdList(); foreach (pwd dpd in dd) { if (dpd.ppName == fname) { dpd.pp = this.textBox1.Text; a += 1; } } }
+                catch (Exception ex) { a = 0; }
+                
+                //实例化一个数据对象
+                if (a == 0)
+                {
+                    pwd p = new pwd() { ppName = fname, pp = this.textBox1.Text };
+                    if (dd == null) { List<pwd> ww = new List<pwd>(); dd = ww; }
+                    dd.Add(p);
+                }                  
+                bool zad=getHost.zAddpwd(dd);
+                if (zad == true) { pwdType = true; } else { pwdType = false; }              
+                TransfEvent1(pwdType);
+               
+                this.Close();
+            }
+            else {
+                pwdType = false;
+                TransfEvent1(pwdType);
+                MessageBox.Show("前后密码不一致或为空");
+            
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pwdType = false;
+            TransfEvent1(pwdType);
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            pwdType = true;
+            TransfEvent1(pwdType);
+            //MessageBox.Show("快捷上锁");
+            this.Close();
+        }
+    }
+}
